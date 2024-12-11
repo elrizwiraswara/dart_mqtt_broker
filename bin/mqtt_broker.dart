583a797d978f9ce81816c4a129057ac3555c5f74
Elriz Wiraswara <elrizwiraswara@gmail.com>
2024-12-11 10:55:21 +0700

    fix import

--- bin/mqtt_broker.dart
+++ bin/mqtt_broker.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:mqtt_broker/mqtt_broker.dart';
+import 'package:dart_mqtt_broker/mqtt_broker.dart';
 
 void main() {
   final mqttBroker = MqttBroker(address: InternetAddress.anyIPv4.address);
